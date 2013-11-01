638353d6012327f3803d46a8ea03406c5016a468
manseka <manseka@localhost>
2013-11-01 12:18:15 -0300

    Probando los commit Creado la 1° Version del archivo config y de paso arranco con Git

--- control/config.php
+++ control/config.php
@@ -5,12 +5,22 @@
  * 
  * 
  */
+require_once 'classDatabase.php';
 
 $config['data'] = array(
 		'host'		=>'localhost',
 		'port'		=> '8889',
 		'usuario' 	=> 'root',
 		'clave'		=> '1842lp',
-		'dbNombre' => 'votoelectronicodb'
+		'dbNombre'      => 'tiendaVirtual'
 		
 );
+
+
+$dsn='mysql:host='.$config['data']['host'].';port='.$config['data']['port'].
+        ';dbname='.$config['data']['dbNombre'];
+
+$opciones ='PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"';
+
+$db = new database($dsn, $config['data']['usuario'], $config['data']['clave'], $opciones);
+
