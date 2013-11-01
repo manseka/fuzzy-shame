0a0633282a4f008c0aa660c3effe4d8b39064b56
manseka <manseka@localhost>
2013-11-01 09:40:39 -0300

    Creado la 1° Version del archivo config y de paso arranco con Git

---
+++ control/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * Configurar Aqui los datos para la conexion.
+ * 
+ * 
+ */
+
+$config['data'] = array(
+		'host'		=>'localhost',
+		'port'		=> '8889',
+		'usuario' 	=> 'root',
+		'clave'		=> '1842lp',
+		'dbNombre' => 'votoelectronicodb'
+		
+);
